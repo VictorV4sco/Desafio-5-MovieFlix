3ce9e108c5d3a442480d981b887e2a96616ab885
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-27 12:42:49 -0300

    FindAllGenres Authorize

--- src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class GenreController {
 	@Autowired
 	private GenreService service;
 	
+	@PreAuthorize("hasAnyRole('ROLE_MEMBER', 'ROLE_VISITOR')")
 	@GetMapping
 	public ResponseEntity<List<GenreDTO>> getAllGenres() {
 		return new ResponseEntity<>(service.findAllGenres(), HttpStatus.OK);
