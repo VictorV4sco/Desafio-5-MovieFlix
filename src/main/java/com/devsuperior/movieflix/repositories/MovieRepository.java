f0cfbc7c744d81c7231ca568499a2f2f4a971039
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-28 15:51:01 -0300

    Movie Tests DONE

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -4,11 +4,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.entities.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 	
 	@Query("SELECT m FROM Movie m WHERE (:id IS NULL OR m.genre.id = :id)")
 	Page<Movie> searchByGenre(Long id, Pageable pageable);
+	
+	@Query("SELECT new com.devsuperior.movieflix.dto.MovieDetailsDTO(m.id, m.title, m.subTitle, m.year, m.imgUrl, m.synopsis, " +
+	           "new com.devsuperior.movieflix.dto.GenreDTO(m.genre.id, m.genre.name)) " +
+	           "FROM Movie m WHERE m.id = :id")
+	    MovieDetailsDTO findMovieDetailsById(@Param("id") Long id);
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -2,10 +2,13 @@ package com.devsuperior.movieflix.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.dto.MovieCardDTO;
 import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.entities.Movie;
@@ -20,13 +23,26 @@ public class MovieService {
 	
 	@Transactional(readOnly = true)
 	public Page<MovieCardDTO> findMoviesByGenre(Pageable pageable, Long id) {
-		Page<Movie> page = repository.searchByGenre(id, pageable);
+		Pageable sortedPageable = PageRequest.of(
+	            pageable.getPageNumber(),
+	            pageable.getPageSize(),
+	            Sort.by("title").ascending()
+	        );
+		
+		Page<Movie> page = repository.searchByGenre(id, sortedPageable);
 		return page.map(x -> new MovieCardDTO(x.getId(), x.getTitle(), x.getSubTitle(), x.getYear(), x.getImgUrl()));
 	}
 	
 	@Transactional(readOnly = true)
 	public MovieDetailsDTO findMovieById(Long id) {
-		Movie movie = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Movie not found"));
-		return new MovieDetailsDTO(movie.getId(), movie.getTitle(), movie.getSubTitle(), movie.getYear(), movie.getImgUrl(), movie.getSynopsis(), null);
+		Movie movie = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Id not found"));
+		return new MovieDetailsDTO(movie.getId(), movie.getTitle(), movie.getSubTitle(), movie.getYear(), movie.getImgUrl(), movie.getSynopsis(),
+				new GenreDTO(movie.getGenre().getId(), movie.getGenre().getName())
+				);
 	}
+	
+//	@Transactional(readOnly = true)
+//    public MovieDetailsDTO findMovieById(Long id) {
+//        return repository.findMovieDetailsById(id);
+//    }
 }
